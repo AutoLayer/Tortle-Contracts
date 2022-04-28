d71135d9281db1c7e86c6260d18c1b60c8dcba03
Alex Pinilla <alexp@glue.digital>
2022-04-28 09:40:23 +0200

    Batch contract - Call methods directly instead if conditions

--- contracts/Batch.sol
+++ contracts/Batch.sol
@@ -49,164 +49,131 @@ contract Batch {
 
     function batchFunctions(Function[] memory _functions) public onlyOwner {
         for (uint256 i = 0; i < _functions.length; i++) {
-            string memory _id = _functions[i].id;
-            string memory _functionName = _functions[i].functionName;
-            address _user = _functions[i].user;
-            string[] memory _arguments = _functions[i].arguments;
-            bool _hasNext = _functions[i].hasNext;
-
-            if (StringUtils.equal(_functionName, 'addFundsForTokens')) {
-                address _token = StringUtils.parseAddr(_arguments[0]);
-                uint256 _amount = StringUtils.safeParseInt(_arguments[1]);
-
-                uint256 amount = nodes.addFundsForTokens(_user, IERC20(_token), _amount);
-                if (_hasNext) {
-                    auxStack.push(amount);
-                }
-
-                emit AddFundsForTokens(_id, _token, amount);
-            } else if (StringUtils.equal(_functionName, 'addFundsForFTM')) {
-                uint256 _amount = StringUtils.safeParseInt(_arguments[0]);
-                if (_hasNext) {
-                    auxStack.push(_amount);
-                }
-
-                emit AddFundsForFTM(_id, _amount);
-            } else if (StringUtils.equal(_functionName, 'split')) {
-                Nodes.SplitStruct memory _splitStruct = splitStruct;
-                _splitStruct.user = _user;
-                _splitStruct.token = StringUtils.parseAddr(_arguments[0]);
-                _splitStruct.firstToken = StringUtils.parseAddr(_arguments[2]);
-                _splitStruct.secondToken = StringUtils.parseAddr(_arguments[3]);
-                _splitStruct.percentageFirstToken = StringUtils.safeParseInt(_arguments[4]);
-                _splitStruct.amountOutMinFirst = StringUtils.safeParseInt(_arguments[5]);
-                _splitStruct.amountOutMinSecond = StringUtils.safeParseInt(_arguments[6]);
-                string memory _firstTokenHasNext = _arguments[7];
-                string memory _secondTokenHasNext = _arguments[8];
-
-                if (auxStack.length > 0) {
-                    _splitStruct.amount = auxStack[auxStack.length - 1];
-                    auxStack.pop();
-                } else {
-                    _splitStruct.amount = StringUtils.safeParseInt(_arguments[1]);
-                }
-
-                (uint256 amountOutToken1, uint256 amountOutToken2) = nodes.split(_splitStruct);
-
-                if (StringUtils.equal(_firstTokenHasNext, 'y')) {
-                    auxStack.push(amountOutToken1);
-                }
-                if (StringUtils.equal(_secondTokenHasNext, 'y')) {
-                    auxStack.push(amountOutToken2);
-                }
-
-                emit Split(_id, _splitStruct.token, _splitStruct.amount, amountOutToken1, amountOutToken2);
-            } else if (StringUtils.equal(_functionName, 'swapTokens')) {
-                address _token = StringUtils.parseAddr(_arguments[0]);
-                uint256 _amount;
-                address _newToken = StringUtils.parseAddr(_arguments[2]);
-                uint256 _amountOutMin = StringUtils.safeParseInt(_arguments[3]);
-
-                if (auxStack.length > 0) {
-                    _amount = auxStack[auxStack.length - 1];
-                    auxStack.pop();
-                } else {
-                    _amount = StringUtils.safeParseInt(_arguments[1]);
-                }
-
-                uint256 amountOut = nodes.swapTokens(_user, IERC20(_token), _amount, _newToken, _amountOutMin);
-                if (_hasNext) {
-                    auxStack.push(amountOut);
-                }
-
-                emit SwapTokens(_id, _token, _amount, _newToken, amountOut);
-            } else if (StringUtils.equal(_functionName, 'liquidate')) {
-                uint256 _tokenArguments = (_arguments.length - 1) / 2;
-
-                IERC20[] memory _tokens = new IERC20[](_tokenArguments);
-                for (uint256 x = 0; x < _tokenArguments; x++) {
-                    address _token = StringUtils.parseAddr(_arguments[x]);
-
-                    _tokens[x] = IERC20(_token);
-                }
-
-                uint256[] memory _amounts = new uint256[](_tokenArguments);
-                uint256 y;
-                for (uint256 x = _tokenArguments; x < _arguments.length - 1; x++) {
-                    uint256 _amount;
-                    if (auxStack.length > 0) {
-                        _amount = auxStack[auxStack.length - 1];
-                        auxStack.pop();
-                    } else {
-                        _amount = StringUtils.safeParseInt(_arguments[x]);
-                    }
-
-                    _amounts[y] = _amount;
-                    y++;
-                }
-
-                address _tokenOutput = StringUtils.parseAddr(_arguments[_arguments.length - 1]);
-
-                uint256 amountOut = nodes.liquidate(_user, _tokens, _amounts, _tokenOutput);
-
-                emit Liquidate(_id, _tokens, _amounts, _tokenOutput, amountOut);
-            } else if (StringUtils.equal(_functionName, 'sendToWallet')) {
-                address _token = StringUtils.parseAddr(_arguments[0]);
-                uint256 _amount;
-
-                if (auxStack.length > 0) {
-                    _amount = auxStack[auxStack.length - 1];
-                    auxStack.pop();
-                } else {
-                    _amount = StringUtils.safeParseInt(_arguments[1]);
-                }
-
-                uint256 amount = nodes.sendToWallet(_user, IERC20(_token), _amount);
-
-                emit SendToWallet(_id, _token, amount);
-            } else if (StringUtils.equal(_functionName, 'depositOnLp')) {
-                uint256 amount0 = StringUtils.safeParseInt(_arguments[3]);
-                uint256 amount1 = StringUtils.safeParseInt(_arguments[4]);
-                if (auxStack.length > 0) {
-                    amount0 = auxStack[auxStack.length - 2];
-                    amount1 = auxStack[auxStack.length - 1];
-                    auxStack.pop();
-                    auxStack.pop();
-                }
-                nodes.depositOnLp(
-                    _user,
-                    StringUtils.parseAddr(_arguments[0]),
-                    StringUtils.parseAddr(_arguments[1]),
-                    StringUtils.parseAddr(_arguments[2]),
-                    amount0,
-                    amount1
-                );
-            } else if (StringUtils.equal(_functionName, 'depositOnFarm')) {
-                (, bytes memory data) = address(nodes).call(abi.encodeWithSignature(_arguments[0], _user, _arguments, auxStack));
-
-                uint8 result = abi.decode(data, (uint8));
-                while (result != 0) {
-                    auxStack.pop();
-                    result--;
-                }
-            } else if (StringUtils.equal(_functionName, 'withdrawFromFarm')) {
-                nodes.withdrawFromFarm(_user, _arguments);
-            } else if (StringUtils.equal(_functionName, 'withdrawFromLp')) {
-                nodes.WithdrawFromLp(_user, _arguments);
-            } else {
-                revert();
-            }
+            (bool success, ) = address(this).call(abi.encodeWithSignature(_functions[i].functionName, _functions[i]));
+            if (!success) revert();
         }
-
-        if (auxStack.length > 0) {
-            deleteAuxStack();
-        }
+        if (auxStack.length > 0) deleteAuxStack();
     }
 
     function deleteAuxStack() private {
         for (uint8 i = 1; i <= auxStack.length; i++) {
             auxStack.pop();
         }
+    }
+
+    function addFundsForFTM(Function memory args) public onlySelf {
+        uint256 amount = StringUtils.safeParseInt(args.arguments[0]);
+        if (args.hasNext) {
+            auxStack.push(amount);
+        }
+
+        emit AddFundsForFTM(args.id, amount);
+    }
+
+    function depositOnFarm(Function memory args) public onlySelf {
+        (, bytes memory data) = address(nodes).call(
+            abi.encodeWithSignature(args.arguments[0], args.user, args.arguments, auxStack)
+        );
+
+        uint8 result = abi.decode(data, (uint8));
+        while (result != 0) {
+            auxStack.pop();
+            result--;
+        }
+    }
+
+    function split(Function memory args) public onlySelf {
+        Nodes.SplitStruct memory _splitStruct = splitStruct;
+        _splitStruct.user = args.user;
+        _splitStruct.token = StringUtils.parseAddr(args.arguments[0]);
+        _splitStruct.firstToken = StringUtils.parseAddr(args.arguments[2]);
+        _splitStruct.secondToken = StringUtils.parseAddr(args.arguments[3]);
+        _splitStruct.percentageFirstToken = StringUtils.safeParseInt(args.arguments[4]);
+        _splitStruct.amountOutMinFirst = StringUtils.safeParseInt(args.arguments[5]);
+        _splitStruct.amountOutMinSecond = StringUtils.safeParseInt(args.arguments[6]);
+        string memory _firstTokenHasNext = args.arguments[7];
+        string memory _secondTokenHasNext = args.arguments[8];
+
+        if (auxStack.length > 0) {
+            _splitStruct.amount = auxStack[auxStack.length - 1];
+            auxStack.pop();
+        } else {
+            _splitStruct.amount = StringUtils.safeParseInt(args.arguments[1]);
+        }
+
+        (uint256 amountOutToken1, uint256 amountOutToken2) = nodes.split(_splitStruct);
+        if (StringUtils.equal(_firstTokenHasNext, 'y')) {
+            auxStack.push(amountOutToken1);
+        }
+        if (StringUtils.equal(_secondTokenHasNext, 'y')) {
+            auxStack.push(amountOutToken2);
+        }
+        emit Split(args.id, _splitStruct.token, _splitStruct.amount, amountOutToken1, amountOutToken2);
+    }
+
+    function addFundsForTokens(Function memory args) public onlySelf {
+        address _token = StringUtils.parseAddr(args.arguments[0]);
+        uint256 _amount = StringUtils.safeParseInt(args.arguments[1]);
+
+        uint256 amount = nodes.addFundsForTokens(args.user, IERC20(_token), _amount);
+        if (args.hasNext) {
+            auxStack.push(amount);
+        }
+
+        emit AddFundsForTokens(args.id, _token, amount);
+    }
+
+    function swapTokens(Function memory args) public onlySelf {
+        address _token = StringUtils.parseAddr(args.arguments[0]);
+        uint256 _amount;
+        address _newToken = StringUtils.parseAddr(args.arguments[2]);
+        uint256 _amountOutMin = StringUtils.safeParseInt(args.arguments[3]);
+
+        if (auxStack.length > 0) {
+            _amount = auxStack[auxStack.length - 1];
+            auxStack.pop();
+        } else {
+            _amount = StringUtils.safeParseInt(args.arguments[1]);
+        }
+
+        uint256 amountOut = nodes.swapTokens(args.user, IERC20(_token), _amount, _newToken, _amountOutMin);
+        if (args.hasNext) {
+            auxStack.push(amountOut);
+        }
+
+        emit SwapTokens(args.id, _token, _amount, _newToken, amountOut);
+    }
+
+    function liquidate(Function memory args) public onlySelf {
+        uint256 _tokenArguments = (args.arguments.length - 1) / 2;
+
+        IERC20[] memory _tokens = new IERC20[](_tokenArguments);
+        for (uint256 x = 0; x < _tokenArguments; x++) {
+            address _token = StringUtils.parseAddr(args.arguments[x]);
+
+            _tokens[x] = IERC20(_token);
+        }
+
+        uint256[] memory _amounts = new uint256[](_tokenArguments);
+        uint256 y;
+        for (uint256 x = _tokenArguments; x < args.arguments.length - 1; x++) {
+            uint256 _amount;
+            if (auxStack.length > 0) {
+                _amount = auxStack[auxStack.length - 1];
+                auxStack.pop();
+            } else {
+                _amount = StringUtils.safeParseInt(args.arguments[x]);
+            }
+
+            _amounts[y] = _amount;
+            y++;
+        }
+
+        address _tokenOutput = StringUtils.parseAddr(args.arguments[args.arguments.length - 1]);
+
+        uint256 amountOut = nodes.liquidate(args.user, _tokens, _amounts, _tokenOutput);
+
+        emit Liquidate(args.id, _tokens, _amounts, _tokenOutput, amountOut);
     }
 
     receive() external payable {}
