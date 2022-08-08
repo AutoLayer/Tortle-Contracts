79eeb22aaeefbdb345e89c1e3ebb15b0443b9b01
Santiago Herrera Blanco <santiago@glue.digital>
2022-08-08 10:38:53 +0200

    #279 MasterChefV2 integration

--- contracts/interfaces/IMasterChef.sol
+++ contracts/interfaces/IMasterChef.sol
@@ -1,60 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 interface IMasterChef {
-    function MaxAllocPoint() external view returns (uint256);
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+    }
 
-    function add(uint256 _allocPoint, address _lpToken) external;
+    struct PoolInfo {
+        IERC20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. BOO to distribute per second.
+        uint256 lastRewardBlock;  // Last block number that SUSHI distribution occurs.
+        uint256 accBooPerShare; // Accumulated BOO per share, times 1e12. See below.
+    }
 
-    function boo() external view returns (address);
-
-    function booPerSecond() external view returns (uint256);
-
-    function deposit(uint256 _pid, uint256 _amount) external;
-
+    function poolInfo(uint256 pid) external view returns (IMasterChef.PoolInfo memory);
+    function userInfo(uint256, address) external view returns (uint256 amount, uint256 rewardDebt);
     function dev(address _devaddr) external;
 
     function devaddr() external view returns (address);
-
     function emergencyWithdraw(uint256 _pid) external;
-
     function getMultiplier(uint256 _from, uint256 _to) external view returns (uint256);
-
     function massUpdatePools() external;
-
     function maxBooPerSecond() external view returns (uint256);
-
     function owner() external view returns (address);
-
     function pendingBOO(uint256 _pid, address _user) external view returns (uint256);
-
-    function poolInfo(uint256)
-        external
-        view
-        returns (
-            address lpToken,
-            uint256 allocPoint,
-            uint256 lastRewardTime,
-            uint256 accBOOPerShare
-        );
-
-    function poolLength() external view returns (uint256);
-
-    function renounceOwnership() external;
-
-    function set(uint256 _pid, uint256 _allocPoint) external;
-
-    function setBooPerSecond(uint256 _booPerSecond) external;
-
-    function startTime() external view returns (uint256);
-
     function totalAllocPoint() external view returns (uint256);
-
-    function transferOwnership(address newOwner) external;
-
-    function updatePool(uint256 _pid) external;
-
-    function userInfo(uint256, address) external view returns (uint256 amount, uint256 rewardDebt);
-
+    function booPerSecond() external view returns (uint256);
+    function deposit(uint256 _pid, uint256 _amount) external;
     function withdraw(uint256 _pid, uint256 _amount) external;
 }
