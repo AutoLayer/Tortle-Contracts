6834a0d81251d9a656a73a91417b0f87410f382e
Santiago Herrera Blanco <santiago@glue.digital>
2022-08-11 11:42:35 +0200

    deployFarms and vaults updated

--- scripts/masterChefV2/deployFarms.js
+++ scripts/masterChefV2/deployFarms.js
@@ -1,11 +1,9 @@
 const { ethers } = require('hardhat')
 const fs = require('fs-extra')
-const farmsListJSON = require('./shortMV2Farms.json')
+const farmList = require('./mv2Farms.json')
 const { WEI } = require('../../test/utils')
 
 const deployMV2 = async () => {
-    const accounts = await ethers.getSigners()
-    const deployer = accounts[0]
     const addresses = await fs.readJSON('./addresses.json')
     const VaultDepositFEE = 10
     const allocPoint = 2000
@@ -20,32 +18,33 @@ const deployMV2 = async () => {
     const _TortleVault = await hre.ethers.getContractFactory('TortleVault')
     const _TortleFarmingsStrategy = await hre.ethers.getContractFactory('TortleFarmingStrategy')
     
-        const rewarder = await (await (await hre.ethers.getContractFactory('ComplexRewarder')).deploy(boo, 0, masterChefV2.address)).deployed()
+    const farm = farmList.find(farm => farm.poolId === "23")
+    const rewarder = await (await (await hre.ethers.getContractFactory('ComplexRewarder')).deploy(farm.address0, 0, masterChefV2.address)).deployed()
 
-        await masterChefV2.add(allocPoint, "0x2fe50aF26C3C1Deb4A149D8C2bD19Dbc1424E13a", rewarder.address, true)
+    await masterChefV2.add(allocPoint, farm.lp, rewarder.address, true)
 
-        tortleVault = await (
-            await _TortleVault.deploy("0x2fe50aF26C3C1Deb4A149D8C2bD19Dbc1424E13a", `${'TORTLE'}-${'DAI'} Spooky Vault`, `tt${'TORTLE'}${'DAI'}`, VaultDepositFEE, WEI(9999999))
-        ).deployed()
-        
-        tortleFarmingStrategy = await (
-            await _TortleFarmingsStrategy.deploy(
-                "0x2fe50aF26C3C1Deb4A149D8C2bD19Dbc1424E13a",
-                23,
-                tortleVault.address,
-                tortleTreasury.address,
-                uniswapRouter.address,
-                masterChefV2.address,
-                boo,
-                wftm
-            )
-        ).deployed()
+    tortleVault = await (
+        await _TortleVault.deploy(farm.lp, `${farm.token0}-${farm.token1} Spooky Vault`, `tt${farm.token0}${farm.token1}`, VaultDepositFEE, WEI(9999999))
+    ).deployed()
+    
+    tortleFarmingStrategy = await (
+        await _TortleFarmingsStrategy.deploy(
+            farm.lp,
+            farm.poolId,
+            tortleVault.address,
+            tortleTreasury.address,
+            uniswapRouter.address,
+            masterChefV2.address,
+            boo,
+            wftm
+        )
+    ).deployed()
 
-        await tortleVault.initialize(tortleFarmingStrategy.address)
+    await tortleVault.initialize(tortleFarmingStrategy.address)
 
-        console.log(`${'TORTLE'}-${'DAI'} --> Farm Created`)
-        console.log("TortleVault Address: ", tortleVault.address)
-        console.log("TortleFarmingStrategy Address: ", tortleFarmingStrategy.address)
+    console.log(`${farm.token0}-${farm.token1} --> Farm Created`)
+    console.log("TortleVault Address: ", tortleVault.address)
+    console.log("TortleFarmingStrategy Address: ", tortleFarmingStrategy.address)
 }
 
 deployMV2()
