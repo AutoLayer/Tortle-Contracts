86ef5e0768a2e9a46d7702b5d87dd0c4db06e78a
Santiago Herrera Blanco <santiago@glue.digital>
2022-11-18 10:03:09 +0000

    Beets interface updated

--- contracts/interfaces/IBeets.sol
+++ contracts/interfaces/IBeets.sol
@@ -20,6 +20,8 @@ struct FundManagement {
     bool toInternalBalance;
 }
 
+enum PoolSpecialization { GENERAL, MINIMAL_SWAP_INFO, TWO_TOKEN }
+
 struct JoinPoolRequest {
     address[] assets;
     uint256[] maxAmountsIn;
@@ -37,6 +39,7 @@ struct ExitPoolRequest {
 interface IBeets {
     function queryBatchSwap(SwapKind kind, BatchSwapStep[] memory swaps, IAsset[] memory assets, FundManagement memory funds) external returns (int256[] memory assetDeltas);
     function batchSwap(SwapKind kind, BatchSwapStep[] memory swaps, IAsset[] memory assets, FundManagement memory funds, int256[] memory limits, uint256 deadline) external payable returns (int256[] memory assetDeltas);
+    function getPool(bytes32 poolId) external view returns (address, PoolSpecialization);
     function joinPool(bytes32 poolId, address sender, address recipient, JoinPoolRequest memory request) external payable;
     function exitPool(bytes32 poolId, address sender, address payable recipient, ExitPoolRequest memory request) external;
 }
