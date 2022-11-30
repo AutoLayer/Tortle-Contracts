676821eb7840e62ca02677706040b7153fdffbac
Santiago Herrera Blanco <santiago@glue.digital>
2022-11-30 12:35:55 +0000

    Bug Fixed: amountOut

--- contracts/SwapsBeets.sol
+++ contracts/SwapsBeets.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.6;
 import "./lib/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./interfaces/IWETH.sol";
 import "./interfaces/IBeets.sol";
 
 contract SwapsBeets is ReentrancyGuard {
@@ -11,10 +12,12 @@ contract SwapsBeets is ReentrancyGuard {
 
     address public immutable owner;
     address public immutable beets;
+    address public immutable FTM;
 
-    constructor(address owner_, address beets_) {
+    constructor(address owner_, address beets_, address ftm_) {
         owner = owner_;
         beets = beets_;
+        FTM = ftm_;
     }
 
     /**
@@ -26,9 +29,11 @@ contract SwapsBeets is ReentrancyGuard {
         IAsset[] memory tokens_,
         BatchSwapStep[] memory batchSwapStep_
     ) public nonReentrant returns (uint256 amountOut) {
-        IERC20(address(tokens_[0])).safeTransferFrom(msg.sender, address(this), batchSwapStep_[0].amount);
-        IERC20(address(tokens_[0])).safeApprove(beets, 0);
-        IERC20(address(tokens_[0])).safeApprove(beets, batchSwapStep_[0].amount);
+        address tokenIn_ = address(tokens_[0]);
+
+        IERC20(tokenIn_).safeTransferFrom(msg.sender, address(this), batchSwapStep_[0].amount);
+        IERC20(tokenIn_).safeApprove(beets, 0);
+        IERC20(tokenIn_).safeApprove(beets, batchSwapStep_[0].amount);
 
         FundManagement memory fundManagement_;
         fundManagement_.sender = address(this);
@@ -39,6 +44,6 @@ contract SwapsBeets is ReentrancyGuard {
         int256[] memory limits_ = IBeets(beets).queryBatchSwap(SwapKind.GIVEN_IN, batchSwapStep_, tokens_, fundManagement_);
 
         int256[] memory amountsOut_ = IBeets(beets).batchSwap(SwapKind.GIVEN_IN, batchSwapStep_, tokens_, fundManagement_, limits_, block.timestamp);
-        amountOut = uint256(amountsOut_[amountsOut_.length - 1]);
+        amountOut = uint256(amountsOut_[amountsOut_.length - 1] * -1);
     }
 }
