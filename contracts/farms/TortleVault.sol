bd3026e2fcd5ef744f830e47c2ddf388d8ad07cd
Santiago Herrera Blanco <santiago@glue.digital>
2022-12-16 08:43:17 +0000

    import deleted

--- contracts/farms/TortleVault.sol
+++ contracts/farms/TortleVault.sol
@@ -7,7 +7,6 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import 'hardhat/console.sol';
 
 error TortleVault__ContractAlreadyInitialized();
 error TortleVault__InvalidAmount();
