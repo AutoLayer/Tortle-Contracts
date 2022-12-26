d8ee1d918fb3b472abe387948c085945a0087ba3
Santiago Herrera Blanco <santiago@glue.digital>
2022-12-26 10:29:12 +0000

    withdraw interface deleted

--- contracts/interfaces/IYearn.sol
+++ contracts/interfaces/IYearn.sol
@@ -3,6 +3,5 @@ pragma solidity ^0.8.6;
 
 interface IYearnVyper {
     function deposit(uint256 _amount, address recipient) external returns(uint256);
-    function withdraw(uint256 maxShares) external returns(uint256);
     function balanceOf(address user) external view returns(uint256);
 }
