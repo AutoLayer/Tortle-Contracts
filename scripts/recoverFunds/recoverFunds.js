ae70ed57856b584551d30c73e128d6a994ebd8c5
Jose María <josemaria@MacBook-Pro-de-Jose.local>
2023-02-07 13:50:16 +0100

    gasLimit added

--- scripts/recoverFunds/recoverFunds.js
+++ scripts/recoverFunds/recoverFunds.js
@@ -18,22 +18,25 @@ const recover = async () => {
         const batchContract = await ethers.getContractAt(batchOldABI, "0x2d8a07D9aacA94954a7474C389362eECF371971F")
 
         let args_ = ["", lpToken, tortleVault, token0, token1, tokenDesired, amountTokenDesiredMin, amount]
+        console.log(args_)
 
-        // const balanceBefore = await nodesContract.getBalance(user, tokenDesired)
-        await batchContract.batchFunctions([{
+        const functionCall = [{
             recipeId: "1",
             id: "1",
             functionName: "withdrawFromFarm",
             user: user,
             arguments: args_,
             hasNext: false
-        }])
+        }]
+
+        // const balanceBefore = await nodesContract.getBalance(user, tokenDesired)
+        await batchContract.batchFunctions(functionCall, {gasLimit: 100000})
         console.log('Withdraw Successful')
         // const balanceAfter = await nodesContract.getBalance(user, tokenDesired)
         // const amountDesired = balanceAfter - balanceBefore
         // await nodesContract.sendToWallet(user, tokenDesired, amountDesired)
         // args_ = [tokenDesired, amountDesired]
-        // await batchContract.batchFunctions([{ 
+        // await batchContract.batchFunctions([{
         //     id: "SendToWallet1",
         //     functionName: "sendToWallet",
         //     user: user,
