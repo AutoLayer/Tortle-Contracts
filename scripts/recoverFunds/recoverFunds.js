35fc1621c31080a0666afcc6ab93645b9dc8fe52
Santiago Herrera Blanco <santiago@glue.digital>
2023-02-08 12:34:35 +0000

    Script updated

--- scripts/recoverFunds/recoverFunds.js
+++ scripts/recoverFunds/recoverFunds.js
@@ -17,31 +17,32 @@ const recover = async () => {
         // const nodesContract = await ethers.getContractAt(nodesOldABI, "0xEDC8736B9686808964C289E03fFab8aa24c7eb56")
         const batchContract = await ethers.getContractAt(batchOldABI, "0x2d8a07D9aacA94954a7474C389362eECF371971F")
 
-        let args_ = ["", lpToken, tortleVault, token0, token1, tokenDesired, amountTokenDesiredMin, amount]
+        // const withdrawArgs_ = ["", lpToken, tortleVault, token0, token1, tokenDesired, amountTokenDesiredMin, amount]
 
-        const functionCall = [{
-            recipeId: "1",
-            id: "1",
-            functionName: "withdrawFromFarm((string,string,string,address,string[],bool))",
-            user: user,
-            arguments: args_,
-            hasNext: false
-        }]
-
-        // const balanceBefore = await nodesContract.getBalance(user, tokenDesired)
-        await batchContract.batchFunctions(functionCall, {gasLimit: 9000000})
-        console.log('Withdraw Successful')
-        // const balanceAfter = await nodesContract.getBalance(user, tokenDesired)
-        // const amountDesired = balanceAfter - balanceBefore
-        // await nodesContract.sendToWallet(user, tokenDesired, amountDesired)
-        // args_ = [tokenDesired, amountDesired]
-        // await batchContract.batchFunctions([{
-        //     id: "SendToWallet1",
-        //     functionName: "sendToWallet",
+        // const functionCall = [{
+        //     recipeId: "1",
+        //     id: "1",
+        //     functionName: "withdrawFromFarm((string,string,string,address,string[],bool))",
         //     user: user,
         //     arguments: args_,
         //     hasNext: false
-        // }])
+        // }]
+
+        // const balanceBefore = await nodesContract.getBalance(user, tokenDesired)
+        // await batchContract.batchFunctions(functionCall, {gasLimit: 9000000})
+        // console.log('Withdraw Successful')
+        // const balanceAfter = await nodesContract.getBalance(user, tokenDesired)
+        // const amountDesired = balanceAfter - balanceBefore
+        // await nodesContract.sendToWallet(user, tokenDesired, amountDesired)
+        const sendToWalletArgs_ = [tokenDesired, "541550000000000000000"]
+        await batchContract.batchFunctions([{
+            recipeId: "1",
+            id: "SendToWallet1",
+            functionName: "sendToWallet((string,string,string,address,string[],bool))",
+            user: user,
+            arguments: sendToWalletArgs_,
+            hasNext: false
+        }])
     }
 }
 
