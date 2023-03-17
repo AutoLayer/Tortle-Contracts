7f0952b8db6e12d8063046d66c10f5d75747407f
Santiago Herrera Blanco <santiago@glue.digital>
2023-03-17 11:35:10 +0000

    Update blockNumber

--- hardhat.config.js
+++ hardhat.config.js
@@ -46,7 +46,7 @@ module.exports = {
       forking: {
         enabled: true,
         url: 'https://rpc.ftm.tools/',
-        blockNumber: 57695098,
+        blockNumber: 57558604,
         allowUnlimitedContractSize: true,
         accounts: [process.env.PRIVATE_KEY],
       }
