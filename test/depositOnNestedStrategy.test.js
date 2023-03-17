88a8ec29ce406957c64ebfbabb6b7b81759cdf55
Santiago Herrera Blanco <santiago@glue.digital>
2023-03-17 12:05:45 +0000

    Add asserts for depositOnNestedStrategy test

--- test/depositOnNestedStrategy.test.js
+++ test/depositOnNestedStrategy.test.js
@@ -25,6 +25,9 @@ describe('Deposit On Nested Strategy', function () {
         const swapAmountOut = swapEvent.args.amountOut.toString()
 
         tx = await nodes.connect(deployer).depositOnNestedStrategy(userAddress, BOO, BOOYearnVault, swapAmountOut)
-        console.log(tx)
+        receipt = await tx.wait()
+        const depositOnNestedStrategyEvent = getEvent(receipt, "DepositOnNestedStrategy")
+
+        assert.equal(depositOnNestedStrategyEvent.args.sharesAmount.toString(), '1729463523840243950', 'Shares amount is not correct.')
     })
 })
