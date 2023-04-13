e9acf3559924bbd2c0dedd62cb8c3c6a9cac54ce
Santiago Herrera Blanco <santiago@glue.digital>
2023-04-13 09:50:30 +0100

    Add GMX interface

---
+++ contracts/interfaces/IGmx.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+interface IGmx {
+    function createIncreasePositionETH(
+        address[] memory _path,
+        address _indexToken,
+        uint256 _minOut,
+        uint256 _sizeDelta,
+        bool _isLong,
+        uint256 _acceptablePrice,
+        uint256 _executionFee,
+        bytes32 _referralCode,
+        address _callbackTarget
+    ) external payable returns (bytes32);
+
+    function createDecreasePosition(
+        address[] memory _path,
+        address _indexToken,
+        uint256 _collateralDelta,
+        uint256 _sizeDelta,
+        bool _isLong,
+        address _receiver,
+        uint256 _acceptablePrice,
+        uint256 _minOut,
+        uint256 _executionFee,
+        bool _withdrawETH,
+        address _callbackTarget
+    ) external payable returns (bytes32);
+}
