0d9e6625f4e1fbbbf1a156b520074f4bd9f2768d
Jose María <josemaria@MacBook-Pro-de-Jose.local>
2023-04-26 14:51:48 +0200

    #662: test mummyFinance deposit operation

--- test/openPerpPosition.test.js
+++ test/openPerpPosition.test.js
@@ -1,7 +1,9 @@
 const { TEST_AMOUNT, calculateAmountWithoutFees } = require('./utils')
+const { ethers } = require('hardhat')
 const { loadFixture } = require('ethereum-waffle')
 const { setUpTests } = require('../scripts/lib/setUpTests')
 const { userAddress, WFTM, USDC } = require('../config')
+const { swapFunction } = require('./functions')
 
 describe('Open Perpetual Position', function () {
     let deployer
@@ -17,10 +19,20 @@ describe('Open Perpetual Position', function () {
     })
 
     it('Open Perpetual Position with FTM', async () => {
-        const amountWithoutFeeInWei = calculateAmountWithoutFees(TEST_AMOUNT)
+        const amountWithoutFeeInWei = calculateAmountWithoutFees('40000000000000000000') // 40 FTM
+        await nodes.connect(deployer).addFundsForFTM(userAddress, "1", { value: amountWithoutFeeInWei })
 
-        const provider = 0
-        tx = await nodes.connect(deployer).openPerpPosition(userAddress, "1", [WFTM, USDC], WFTM, amountWithoutFeeInWei, /*sizeDelta,*/ false, amountWithoutFeeInWei, 0, provider)
+        let path_ = [WFTM] // FTM/FTM
+        let indexToken_ = WFTM // FTM
+        let isLong_ = 'true'
+        let amount_ = amountWithoutFeeInWei
+        let leverage = 2
+        let indexTokenPrice = BigInt(0.4281 * 1000000000000000000000000000000 * leverage).toString()// 1 entryToken USD price multiplied * 10^30 -> 0.47$ FTM ^10 ^30
+        let executionFee_ = '200000000000000000'
+        let amountOutMin_ = '0'
+        let provider_ = 0
+        const args = ethers.utils.defaultAbiCoder.encode(['address[]', 'address', 'bool', 'uint256', 'uint256', 'uint256', 'uint256', /*'uint8',*/ 'uint8'], [path_, indexToken_, isLong_, amount_, indexTokenPrice, executionFee_, amountOutMin_, /*leverage,*/ provider_]);
+        tx = await nodes.connect(deployer).openPerpPosition(userAddress, "1", args)
         receipt = await tx.wait()
         console.log(receipt)
     })
