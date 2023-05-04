302e0f748333a63532d353d5e35cc0ce1f04f7be
Santiago Herrera Blanco <santiago@glue.digital>
2023-05-04 12:19:17 +0100

    reverted4farmsRecipe test

--- test/functions.js
+++ test/functions.js
@@ -1,7 +1,7 @@
 const { ethers } = require('hardhat')
 const { getEvent } = require('./utils')
 
-const swapFunction = async (deployer, userAddress, inputToken, amountIn, outputToken) => {
+const swapFunction = async (nodes, deployer, userAddress, inputToken, amountIn, outputToken) => {
     const tx = await nodes.connect(deployer).swapTokens(userAddress, "0", [inputToken, outputToken], amountIn, "0", [])
     const receipt = await tx.wait()
     const swapEvent = getEvent(receipt, "Swap")
@@ -9,7 +9,7 @@ const swapFunction = async (deployer, userAddress, inputToken, amountIn, outputT
     return swapEvent.args.amountOut.toString()
 }
 
-const splitFunction = async (deployer, userAddress, inputToken, amountIn, outputToken1, outputToken2) => {
+const splitFunction = async (nodes, deployer, userAddress, inputToken, amountIn, outputToken1, outputToken2) => {
     const args = ethers.utils.defaultAbiCoder.encode(
         ['address', 'address[]', 'address[]', 'uint256', 'uint256[]', 'uint8[]'],
         [userAddress, [inputToken, outputToken1], [inputToken, outputToken2], amountIn, [5000, 0, 0], [0, 0]]
