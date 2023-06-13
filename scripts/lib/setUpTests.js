c9eed184b49f5883b2106786f64251d554d71086
Santiago Herrera Blanco <santiago@glue.digital>
2023-06-13 09:19:34 +0100

    Add env variable to change deploy and tests network between Fantom and Arbitrum

--- scripts/lib/setUpTests.js
+++ scripts/lib/setUpTests.js
@@ -1,4 +1,5 @@
-const { deployMainNet } = require('./deployMainnet')
+const { deployMainNet } = require('./deployMainNet')
+const { deployArbitrumMainNet } = require('./deployArbitrumMainNet')
 const { impersonateAccount }  = require('@nomicfoundation/hardhat-network-helpers')
 const { TEST_AMOUNT } = require('../../test/utils')
 const { deployerAddress, userAddress } = require('../../config')
@@ -7,7 +8,10 @@ const setUpTests = async () => {
     await impersonateAccount(deployerAddress)
     const deployer = await ethers.getSigner(deployerAddress)
 
-    const contractsAddresses = await deployMainNet({noWait: true, deployer})
+    let contractsAddresses
+    if (process.env.NETWORK === 'Fantom') contractsAddresses = await deployMainNet({noWait: true, deployer})
+    else contractsAddresses = await deployArbitrumMainNet({noWait: true, deployer})
+
     nodes = await ethers.getContractAt('Nodes', contractsAddresses.ProxyNodes)
 
     await nodes.connect(deployer).addFundsForFTM(userAddress, "1", { value: TEST_AMOUNT })
