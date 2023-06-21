3b2390c968d5471b963485bd435e7e2c0467c6ee
Santiago Herrera Blanco <santiago@glue.digital>
2023-06-21 11:48:50 +0200

    Update masterChef interface

--- contracts/interfaces/IMasterChef.sol
+++ contracts/interfaces/IMasterChef.sol
@@ -30,3 +30,7 @@ interface IMasterChef {
     function deposit(uint256 _pid, uint256 _amount) external;
     function withdraw(uint256 _pid, uint256 _amount) external;
 }
+
+interface IMasterChefV2 {
+    function deposit(uint256 _pid, uint256 _amount, address _to) external;
+}
