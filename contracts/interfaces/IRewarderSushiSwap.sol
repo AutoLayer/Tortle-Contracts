29e174ddbfcbd62acd64a6d9b47b886067bf3f03
Santiago Herrera Blanco <santiago@glue.digital>
2023-08-02 13:09:41 +0200

    #805 Add function to the rewarder interface

--- contracts/interfaces/IRewarderSushiSwap.sol
+++ contracts/interfaces/IRewarderSushiSwap.sol
@@ -6,4 +6,5 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 interface IRewarderSushiSwap {
     function onSushiReward(uint256 pid, address user, address recipient, uint256 sushiAmount, uint256 newLpAmount) external;
     function pendingTokens(uint256 pid, address user, uint256 sushiAmount) external view returns (IERC20[] memory, uint256[] memory);
+    function pendingToken(uint256 _pid, address _user) external view returns (uint256 pending);
 }
